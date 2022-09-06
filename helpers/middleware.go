95d79f0a435d555f8cb25a0ec792cba36e002707
mohamad sadegh mohamadi <82243525+sadegh-msm@users.noreply.github.com>
2022-09-06 13:41:43 +0430

    Update middleware.go

--- helpers/middleware.go
+++ helpers/middleware.go
@@ -7,8 +7,14 @@ import (
 
 func MaxAllowedMiddleware(n uint) mux.MiddlewareFunc {
 	sem := make(chan struct{}, n)
-	acquire := func() { sem <- struct{}{} }
-	release := func() { <-sem }
+	
+	acquire := func() { 
+		sem <- struct{}{} 
+	}
+	
+	release := func() {
+		<-sem 
+	}
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
